c57ddf6a9ce7a4e1c573272c42e8f72f0b3bf364
mr-rush <woot@local>
2018-05-30 16:54:41 +1000

    Fixed egress SG.

--- terraform/confluent.tf
+++ terraform/confluent.tf
@@ -49,6 +49,20 @@ resource "aws_security_group" "perceive_confluent_sg" {
     cidr_blocks = ["10.1.1.0/24"]
   }
 
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   lifecycle {
     create_before_destroy = true
   }
