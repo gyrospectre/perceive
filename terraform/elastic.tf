2ed72920f1b29442ada04fe71245010cd373f806
mr-rush <woot@local>
2018-05-31 19:47:59 +1000

    ES to t3.small.

--- terraform/elastic.tf
+++ terraform/elastic.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "elastic" {
   ami      = "${data.aws_ami.perceive_base_ami.id}"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   key_name = "main"
   vpc_security_group_ids = ["${aws_security_group.perceive_elastic_sg.id}"]
   lifecycle {
